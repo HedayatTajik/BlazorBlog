25304b4c7bf904d41b765236a59db60b31ab0bcd
Hedayatullah Tajik <hedayatullah.tajik2@sma.de>
2023-02-15 11:30:04 +0100

    Add project files.

---
+++ BlazorBlog/Shared/BlogPost.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBlog.Shared
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
